a43355db11943732e10e63ec9a016f3ec38089f9
ConnieBerrios <connieberrios@live.com>
2021-09-02 07:23:04 +0000

    Tarea incompleta

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite #Aqui importar elementos
 #from models import Person
 
 app = Flask(__name__)
@@ -32,14 +32,30 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    #en la variable all_users estoy consultando a la base de datos por todos los registros de la tabla users
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    all_users = User.query.get(2)
+    #estoy almacenando dentro de una listalist()
+    # realizando un map(), donde un map ejecuta una instruccion por cada registro de un usuario en mi base de datos
+    #y lambda es lo mismo que una funcion ()=>{} que ejecuta por cada registro de la base de datos (X) y ejecutara su funcion serialize() para devolver los datos que quiero
 
-    return jsonify(response_body), 200
+    all_users = list(map(lambda x: x.serialize(), all_users))
+    #retorno todos los usuarios
+    return jsonify(all_users), 200
+     
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+#     return jsonify(response_body), 200
+
+# # this only runs if `$ python src/main.py` is executed
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3000))
+#     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+@app.route('/favoritos', methods=['GET'])
+def allFavoritos():
+    resultado ={"Mensaje ": "Aca iran todos los favoritos"}
+    return jsonify(resultado)
+
--- src/models.py
+++ src/models.py
@@ -6,14 +6,54 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nameFavorite = db.Column(db.String(30), unique= False, nullable=False )
+    typeFavorite = db.Column(db.String(30), unique=False, nullable=False)
+    relationUser = db.relationship("User")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "nameFavorite": self.nameFavorite,
+            "typeFavorite": self.typeFavorite
+        }
+
+class Planet(db.Model):
+    
+    id = db.Column(db.Integer, )
+    name = db.Column(db.String(50), primary_key=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            
+        }
+
+class Characters(db.Model):
+  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30),  nullable = False)
+    homeworld = db.Column(db.String(50), db.ForeignKey('planet.name'))
+    relationPlanet = db.relationship("Planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "homeworld": self.homeworld
+            
         }
